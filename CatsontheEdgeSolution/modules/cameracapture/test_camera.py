8bd2bed36ff53e0ea452a1937358a8f941de278d
Elena Terenzi <eterenzi@microsoft.com>
2018-12-10 09:01:33 +0100

    initial commit

--- CatsontheEdgeSolution/modules/cameracapture/test_camera.py
+++ CatsontheEdgeSolution/modules/cameracapture/test_camera.py
@@ -1,20 +1,6 @@
-'''import time
-import picamera
-import numpy as np
-import cv2
-
-res = (640,480,3)
-camera = picamera.PiCamera()
-camera.resolution = res[:2]
-camera.framerate = 24
-time.sleep(2)
-frame = np.empty((res[0] * res[1] * res[2]), dtype=np.uint8)
-camera.capture(frame, 'bgr')
-frame = frame.reshape(res)'''
-
 ## test it on your Raspberry. 
 # Note that if the cameracapture module is running 
-# you will receive a MMAL error,
+# you will receive a MMAL error.
 # this is because the camera is already in use, 
 # so stop the cameracapture module before running this code
 from time import sleep
